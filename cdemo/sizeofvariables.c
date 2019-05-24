a226ef1399199d2ac24a07e0200e35485d9d22e7
Joseph Kim <josephsk1020@gmail.com>
2019-05-24 17:26:09 +0000

    sizeofvariables

---
+++ cdemo/sizeofvariables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 545;
+  char b = 'p';
+  float c = 3000;
+  double d = 3000.1;
+  // print value and size of an int variable
+  printf("int a size: %d bytes\n", sizeof(a));
+  printf("char b size: %d bytes\n",  sizeof(b));
+  printf("float c size: %d bytes\n", sizeof(c));
+  printf("double d size: %d bytes\n", sizeof(d));
+
+
+}
+
